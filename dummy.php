0c8b0d40a35120ce3fb489ed5d97c1f3d20e289b
Dirge <fuaddabbad@gmail.com>
2025-07-05 08:06:57 +0700

    first-commit

---
+++ dummy.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Dummy
+ * Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, cum rerum laudantium perferendis maiores delectus vitae, fuga reiciendis totam aliquid, ea ab repellat asperiores odit?
+ * Plugin URI: https://github.com/Dirga36
+ * Version: 1.0.0
+ * Author: Dirga
+ * Author URI: https://github.com/Dirga36
+ * Text Domain: dummy
+ *
+ * @package Dummy
+ * @category Core
+ *
+ * Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse labore exercitationem eos modi delectus vel laudantium eum beatae doloribus debitis enim dolore laboriosam iusto, corrupti accusantium sit vero voluptatibus quis culpa quos reiciendis! Voluptates animi natus quaerat consequuntur nemo tenetur architecto qui itaque veniam possimus rerum odio sunt dolorem enim eaque eius debitis, reprehenderit fuga facilis accusamus maiores ipsa quos optio iure? Enim ut soluta ad tenetur porro asperiores rem excepturi deleniti nisi doloribus ullam qui id explicabo placeat consectetur, consequuntur delectus laboriosam optio obcaecati dolor. Beatae quisquam repellendus dignissimos rem laudantium vitae adipisci, perspiciatis non hic ab ex.
+**/
