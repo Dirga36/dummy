0f7d4c4ec0be0002a34827fe6b1a243762df5256
Dirge <fuaddabbad@gmail.com>
2025-07-06 15:53:27 +0700

    2

--- dummy.php
+++ dummy.php
@@ -7,12 +7,7 @@
  * Version: 1.0.0
  * Author: Dirga
  * Author URI: https://github.com/Dirga36
- * Text Domain: dummy
- *
- * @package Dummy
- * @category Core
- *
- * Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga esse labore exercitationem eos modi delectus vel laudantium eum beatae doloribus debitis enim dolore laboriosam iusto, corrupti accusantium sit vero voluptatibus quis culpa quos reiciendis! Voluptates animi natus quaerat consequuntur nemo tenetur architecto qui itaque veniam possimus rerum odio sunt dolorem enim eaque eius debitis, reprehenderit fuga facilis accusamus maiores ipsa quos optio iure? Enim ut soluta ad tenetur porro asperiores rem excepturi deleniti nisi doloribus ullam qui id explicabo placeat consectetur, consequuntur delectus laboriosam optio obcaecati dolor. Beatae quisquam repellendus dignissimos rem laudantium vitae adipisci, perspiciatis non hic ab ex.
+ * Text Domain: dummy-plug
 **/
 
 if ( !defined('ABSPATH') )
@@ -21,7 +16,7 @@ if ( !defined('ABSPATH') )
     exit;
 }
 
-class dummy
+class SimpleContactForm
 {
     public function __construct()
     {
@@ -43,8 +38,9 @@ class dummy
             'capability_type' => 'manage_options',
             'menu_icon' => 'dashicons-media-text',
         );
+
         register_post_type('simple_contact_form', $args);
     }
 }
 
-new dummy();
+new SimpleContactForm;
